5ac3f7823a2bf0e45d55cc05747eb6d350ba431a
jkeiser <jkeiser@opscode.com>
2012-12-08 10:45:58 -0800

    Add rename ability to PUT

--- spec/tiny_chef_server.rb
+++ spec/tiny_chef_server.rb
@@ -60,7 +60,7 @@ class TinyChefServer < Rack::Server
         [ '/cookbooks/*/*', CookbookVersionEndpoint.new(data) ],
         [ '/data', DataBagsEndpoint.new(data) ],
         [ '/data/*', RestListEndpoint.new(data, 'id') ],
-        [ '/data/*/*', RestObjectEndpoint.new(data) ],
+        [ '/data/*/*', RestObjectEndpoint.new(data, 'id') ],
         [ '/environments', RestListEndpoint.new(data) ],
         [ '/environments/*', EnvironmentEndpoint.new(data) ],
         [ '/environments/*/cookbooks', EnvironmentCookbooksEndpoint.new(data) ],
@@ -216,7 +216,7 @@ EOM
     end
 
     def error(response_code, error)
-      json_response(response_code, {"error" => error})
+      json_response(response_code, {"error" => [error]})
     end
 
     def json_response(response_code, json)
@@ -281,14 +281,32 @@ EOM
 
   # Typical REST leaf endpoint (/roles/NAME or /data/BAG/NAME)
   class RestObjectEndpoint < RestBase
+    def initialize(data, identity_key = 'name')
+      super(data)
+      @identity_key = identity_key
+    end
+
+    attr_reader :identity_key
+
     def get(request)
       already_json_response(200, get_data(request))
     end
     def put(request)
       # We grab the old body to trigger a 404 if it doesn't exist
       old_body = get_data(request)
-      get_data(request, request.rest_path[0..-2])[request.rest_path[-1]] = request.body
-      already_json_response(200, request.body)
+      request_json = JSON.parse(request.body, :create_additions => false)
+      key = request_json[identity_key] || request.rest_path[-1]
+      container = get_data(request, request.rest_path[0..-2])
+      # If it's a rename, check for conflict and delete the old value
+      rename = key != request.rest_path[-1]
+      if rename
+        if container.has_key?(key)
+          return error(409, "Cannot rename '#{request.rest_path[0..-2]}' to '#{key}': '#{key}' already exists")
+        end
+        container.delete(request.rest_path[-1])
+      end
+      container[key] = request.body
+      already_json_response(rename ? 201 : 200, request.body)
     end
     def delete(request)
       key = request.rest_path[-1]
